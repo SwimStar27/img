638ed5dd71f59ed06e9280a7b1d62b44fb291d23
SwimStar27 <94149915+SwimStar27@users.noreply.github.com>
2022-05-01 14:29:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,23 @@ function brownie(){
 
 console.log('This is ml5',ml5.version);
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8Di17p2uP/model.json',modelLoaded);
+
+function modelLoaded(){
+ console.log('Coding');
+}
+
+function cakepop(){
+    myimg=document.getElementById('random3.0');
+    classifier.classify(myimg,gotresult);
+}
+
+function gotresult(error,results){
+     if(error){
+         console.error(error);
+     }
+     else{
+         console.log(results);
+         document.getElementById("object_name").innerHTML=results[0].label;
+         document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+     }
+}
